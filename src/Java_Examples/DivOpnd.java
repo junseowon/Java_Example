b76449682be50436741044deed3c2ba1c5ed1a13
junseowon <junseowon@gmail.com>
2023-01-30 13:17:59 +0900

    DivOpnd 나눗셈 연산자.

---
+++ src/Java_Examples/DivOpnd.java
@@ -0,0 +1,9 @@
+package Java_Examples;
+
+public class DivOpnd {
+    public static void main(String[] args) {
+        System.out.println("정수형 나눗셈 : " + 7 / 3);
+        System.out.println("실수형 나눗셈 : " + 7.0f / 3.0f);
+        System.out.println("형 변환 나눗셈 : " + (float) 7 / 3);
+    }
+}
