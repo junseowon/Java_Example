7f070ca53c8444f6e679dbafe8ace1a66aa14c53
junseowon <junseowon@gmail.com>
2023-01-30 12:40:58 +0900

    CastingOperation 8행에서 ch1은 char형이고 num1은 int형이다. char형은 int형으로 자동 형 변환이 가능하므로 ch1에 저장된 값이 형 변환되어 num1에 저장된다. 9행은 결과적으로는 8행과 차이가 없다. 그러나 형 변환이 발생된다는 사실을 코드상에서 표현한 것이 되기 때문에 이것이 더 권장하는 코드 구현 방법이다. 자동으로 형 변환이 발생하는 위치에 명시적으로 형 변환이 됨을 표현할 것.

--- src/Java_Examples/CastingOperation.java
+++ src/Java_Examples/CastingOperation.java
@@ -3,7 +3,7 @@ package Java_Examples;
 public class CastingOperation {
     public static void main(String[] args) {
         char ch1 = 'A';
-        char ch2 = 'z';
+        char ch2 = 'Z';
 
         int num1 = ch1;
         int num2 = (int)ch2;
