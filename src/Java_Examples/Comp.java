d8b839a74317e7ce5220e4d1464cb47a897a9ac9
junseowon <junseowon@gmail.com>
2023-01-30 13:28:19 +0900

    Comp 복합 대입 연산자.

---
+++ src/Java_Examples/Comp.java
@@ -0,0 +1,15 @@
+package Java_Examples;
+
+public class Comp {
+    public static void main(String[] args) {
+        double e = 3.1;
+        e += 2.1;
+        e *= 2;
+
+        int n = 5;
+        n *= 2.2;
+
+        System.out.println(e);
+        System.out.println(n);
+    }
+}
