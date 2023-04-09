d34ac5592811f7d81ebdd00d5deac49e687c6fb0
junseowon <106894687+junseowon@users.noreply.github.com>
2023-04-09 20:47:18 +0900

    Update HelloWorld.java

--- src/Java_Examples/HelloWorld.java
+++ src/Java_Examples/HelloWorld.java
@@ -2,6 +2,6 @@ package Java_Examples;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");//Hello world!출력
     }
 }
